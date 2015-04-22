7b41734f8ccced192d2fd2256423401029ff9922
Ariel Gorfinkel <arielg@novideasoft.com>
2015-04-22 11:39:07 +0300

    Added pom.xml and empty ApexTokenizer

---
+++ pmd-apex/src/main/java/net/sourceforge/pmd/cpd/ApexTokenizer.java
@@ -0,0 +1,13 @@
+package net.sourceforge.pmd.cpd;
+
+import java.io.IOException;
+
+public class ApexTokenizer implements Tokenizer{
+
+	@Override
+	public void tokenize(SourceCode arg0, Tokens arg1) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
